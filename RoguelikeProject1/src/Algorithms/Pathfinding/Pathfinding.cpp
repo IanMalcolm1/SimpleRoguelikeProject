e679e03336600f7821795ea6721a3b5fcf8e1a6c
IanMalcolm1 <92656592+IanMalcolm1@users.noreply.github.com>
2023-01-17 20:41:44 -0800

    Fixed my fix to my bresenham line pathfinding implementation

--- RoguelikeProject1/src/Algorithms/Pathfinding/Pathfinding.cpp
+++ RoguelikeProject1/src/Algorithms/Pathfinding/Pathfinding.cpp
@@ -22,9 +22,18 @@ void Pathfinding::bresenhamLine(TileCoordinates startTile, TileCoordinates endTi
 		loopIncrement = startTile.x < endTile.x ? 1 : -1;
 		secondaryIncrement = startTile.y < endTile.y ? 1 : -1;
 
-		for (int x = startTile.x; x != endTile.x + loopIncrement; x += loopIncrement) {
-			if (x == startTile.x && y == startTile.y) { continue;  } //hacky
+		//one iteration outside to skip start tile
+		if (2 * (modifiedError + deltaY) < deltaX) {
+			modifiedError += deltaY;
+		}
+		else {
+			y += secondaryIncrement;
+			modifiedError += deltaY - deltaX;
+		}
+		int x = startTile.x;
+		x += loopIncrement;
 
+		for (x; x != endTile.x + loopIncrement; x += loopIncrement) {
 			if (!localMap->isTraversibleAt({ x,y })) {
 				break;
 			}
@@ -47,8 +56,19 @@ void Pathfinding::bresenhamLine(TileCoordinates startTile, TileCoordinates endTi
 		loopIncrement = startTile.y < endTile.y ? 1 : -1;
 		secondaryIncrement = startTile.x < endTile.x ? 1 : -1;
 
-		for (int y = startTile.y; y != endTile.y + loopIncrement; y += loopIncrement) {
-			if (x == startTile.x && y == startTile.y) { continue; } //still hacky
+		//one iteration outside to skip first tile
+		if (2 * modifiedError + deltaX < deltaY) {
+			modifiedError += deltaX;
+		}
+		else {
+			x += secondaryIncrement;
+			modifiedError += deltaX - deltaY;
+		}
+		int y = startTile.y;
+		y += loopIncrement;
+		
+
+		for (y; y != endTile.y + loopIncrement; y += loopIncrement) {
 			if (!localMap->isTraversibleAt({ x,y })) {
 				break;
 			}
